73f82bd0dca18c1cdda9d893e10237cd54148592
Engr-Jalal-Saleem <124458089+Engr-Jalal-Saleem@users.noreply.github.com>
2024-03-14 23:11:34 +0500

    Updating Classes methods

--- Classes/1.2)Input_Attributes_Classes.cpp
+++ Classes/1.2)Input_Attributes_Classes.cpp
@@ -1,3 +1,10 @@
+/**
+ * The code defines a C++ class representing a YouTube channel with properties like name, owner name,
+ * subscribers count, and a list of published video titles, along with methods to input and display
+ * channel information.
+ * @return The main function in the code returns an integer value of 0. This is a common practice in
+ * C++ programs to indicate that the program executed successfully without any errors.
+ */
 #include<iostream>
 #include<list>
 using namespace std;
@@ -8,38 +15,37 @@ class YouTube_Channel{
     string OwnerName;
     int SubscribersCount;
     list<string> PublishedVideoTitles;
+    void getInfo(){
+        cout<<"Enter the name of the YouTube channel: ";
+        cin>>Name;
+        cout<<"Enter the name of the owner of the YouTube channel: ";
+        cin>>OwnerName;
+        cout<<"Enter the number of subscribers of the YouTube channel: ";
+        cin>>SubscribersCount;
+        cout<<"Enter the number of videos published by the YouTube channel: ";
+        int n;
+        cin>>n;
+        cout<<"Enter the titles of the videos: ";
+        for(int i=0; i<n; i++){
+            string title;
+            cin>>title;
+            PublishedVideoTitles.push_back(title);
+        }
+    }
+    void showInfo(){
+        cout<<"Name: "<<Name<<endl;
+        cout<<"Owner Name: "<<OwnerName<<endl;
+        cout<<"Subscribers: "<<SubscribersCount<<endl;
+        cout<<"Videos: "<<endl;
+        for(string vids: PublishedVideoTitles){
+            cout<<vids<<endl;
+        }
+    }
 };
+
 int main(){
     YouTube_Channel ytChannel;
-    string Name, OwnerName;
-    int SubscribersCount;
-    list<string> PublishedVideoTitles;
-    cout<<"Enter the name of the YouTube channel: ";
-    cin>>Name;
-    cout<<"Enter the name of the owner of the YouTube channel: ";
-    cin>>OwnerName;
-    cout<<"Enter the number of subscribers of the YouTube channel: ";
-    cin>>SubscribersCount;
-    cout<<"Enter the number of videos published by the YouTube channel: ";
-    int n;
-    cin>>n;
-    cout<<"Enter the titles of the videos: ";
-    for(int i=0; i<n; i++){
-        string title;
-        cin>>title;
-        PublishedVideoTitles.push_back(title);
-    }
-
-    ytChannel.Name = Name;
-    ytChannel.OwnerName = OwnerName;
-    ytChannel.SubscribersCount = SubscribersCount;
-    ytChannel.PublishedVideoTitles = PublishedVideoTitles;
-    cout<<"Name: "<<ytChannel.Name<<endl;
-    cout<<"Owner Name: "<<ytChannel.OwnerName<<endl;
-    cout<<"Subscribers: "<<ytChannel.SubscribersCount<<endl;
-    cout<<"Videos: "<<endl;
-    for(string vids: ytChannel.PublishedVideoTitles){
-        cout<<vids<<endl;
-    }
+    ytChannel.getInfo();
+    ytChannel.showInfo();
     return 0;
 }
